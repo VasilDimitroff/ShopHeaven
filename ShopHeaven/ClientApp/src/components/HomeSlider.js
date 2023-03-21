855efb262d36d1746391cb193fa2938f14203088
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2023-03-21 15:50:24 +0200

    wrong

--- ShopHeaven/ClientApp/src/components/HomeSlider.js
+++ ShopHeaven/ClientApp/src/components/HomeSlider.js
@@ -5,7 +5,7 @@ import {
   Grid,
   List,
   ListItemButton,
-  Paper,
+  ListItemText,
   Divider,
   Fade,
   Button,
@@ -362,7 +362,14 @@ export default function HomeSlider() {
                   </Submenu>
                 </Fade>
               </List>
+<<<<<<< HEAD
             </MainMenuWrapper>
+=======
+            </Grid>
+            <Grid item xs={0} md={7}>
+              <div>xs=6 md=8</div>
+            </Grid>
+>>>>>>> parent of a434c13 (wrong slider install)
           </Grid>
           <Grid xs={12} lg={9} >
               <CarouselSlider />
