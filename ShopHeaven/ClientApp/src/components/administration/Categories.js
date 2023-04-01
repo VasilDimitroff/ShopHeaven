db40880c7a9483ed1a54a6bad06ec50d05f0e6bd
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2023-04-01 18:53:28 +0300

    modal for category creating introduced

--- ShopHeaven/ClientApp/src/components/administration/Categories.js
+++ ShopHeaven/ClientApp/src/components/administration/Categories.js
@@ -1,4 +1,4 @@
-import { React, useState, Fragment, Chip } from "react";
+import { React, useState, Fragment } from "react";
 import {
   Box,
   Button,
@@ -12,6 +12,10 @@ import {
   TableBody,
   TableHead,
   TableContainer,
+  Modal,
+  Fade,
+  Zoom,
+  Backdrop,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { theme } from "../../theme";
@@ -21,45 +25,40 @@ import {
   Edit,
   Delete,
   AddCircle,
-  RemoveCircle,
-  Close,
 } from "@mui/icons-material";
-import { style } from "@mui/system";
 
 function Row(props) {
   const [open, setOpen] = useState(false);
 
-  const StyledImage = styled('img')({
+  const StyledImage = styled("img")({
     width: "30px",
-    height: "30px"
+    height: "30px",
   });
 
   const StyledButtonBox = styled(Box)({
-    marginTop: theme.spacing(2)
+    marginTop: theme.spacing(2),
   });
 
   const CategoryNameTableCell = styled(TableCell)({
     fontWeight: 500,
-    fontSize: 18
-  })
+    fontSize: 18,
+  });
 
-  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({}));
 
-  }));
-  
   const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
+    "&:nth-of-type(odd)": {
       backgroundColor: theme.palette.action.hover,
     },
     // hide last border
-    '&:last-child td, &:last-child th': {
+    "&:last-child td, &:last-child th": {
       border: 0,
     },
   }));
 
   const StyledButton = styled(Button)({
-    boxShadow: "none"
-  })
+    boxShadow: "none",
+  });
 
   function renderCategoryProductsCount() {
     return props.subcategories.reduce(function (a, b) {
@@ -67,29 +66,48 @@ function Row(props) {
     }, 0);
   }
 
-  function renderSubcategories(){
+  function renderSubcategories() {
     return props.subcategories.map((subcategory) => (
-        <StyledTableRow key={subcategory.id}>
-          <StyledTableCell align="center">
-            {subcategory.id}
-          </StyledTableCell>
-          <StyledTableCell component="th" scope="row">
-            {subcategory.name}
-          </StyledTableCell>
-          <StyledTableCell align="center">
-            <StyledImage src={subcategory.image} />
-          </StyledTableCell>
-          <StyledTableCell align="center">{subcategory.productsCount}</StyledTableCell>
-          <StyledTableCell align="center">{subcategory.createdBy}</StyledTableCell>
-          <StyledTableCell align="center"> <StyledButton color="warning" variant="contained" size="small" startIcon={<Edit />}>EDIT</StyledButton></StyledTableCell>
-          <StyledTableCell align="center"> <StyledButton color="error" variant="contained" size="small" startIcon={<Delete />}>DELETE</StyledButton> </StyledTableCell>
-        </StyledTableRow>
-      ))
+      <StyledTableRow key={subcategory.id}>
+        <StyledTableCell align="center">{subcategory.id}</StyledTableCell>
+        <StyledTableCell component="th" scope="row">
+          {subcategory.name}
+        </StyledTableCell>
+        <StyledTableCell align="center">
+          {subcategory.productsCount}
+        </StyledTableCell>
+        <StyledTableCell align="center">
+          {subcategory.createdBy}
+        </StyledTableCell>
+        <StyledTableCell align="center">
+          {" "}
+          <StyledButton
+            color="warning"
+            variant="contained"
+            size="small"
+            startIcon={<Edit />}
+          >
+            EDIT
+          </StyledButton>
+        </StyledTableCell>
+        <StyledTableCell align="center">
+          {" "}
+          <StyledButton
+            color="error"
+            variant="contained"
+            size="small"
+            startIcon={<Delete />}
+          >
+            DELETE
+          </StyledButton>{" "}
+        </StyledTableCell>
+      </StyledTableRow>
+    ));
   }
 
   return (
     <Fragment>
-      <TableRow sx={{ "& > *": { borderBottom: "unset", }}}>
+      <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
         <TableCell>
           <IconButton
             aria-label="expand row"
@@ -100,19 +118,43 @@ function Row(props) {
           </IconButton>
         </TableCell>
         <TableCell align="center">{props.category.id}</TableCell>
-        <CategoryNameTableCell component="th" scope="row">{props.category.name}</CategoryNameTableCell>
-        <TableCell align="center"><StyledImage src={props.category.image} /></TableCell>
+        <CategoryNameTableCell component="th" scope="row">
+          {props.category.name}
+        </CategoryNameTableCell>
         <TableCell align="center">{renderCategoryProductsCount()}</TableCell>
         <TableCell align="center">{props.subcategories.length}</TableCell>
         <TableCell align="center">{props.category.createdBy}</TableCell>
-        <TableCell align="center"><StyledButton color="warning" variant="contained" size="small" startIcon={<Edit />}>EDIT</StyledButton></TableCell>
-        <TableCell align="center"><StyledButton color="error" variant="contained" size="small" startIcon={<Delete />}>DELETE</StyledButton></TableCell>
+        <TableCell align="center">
+          <StyledButton
+            color="warning"
+            variant="contained"
+            size="small"
+            startIcon={<Edit />}
+          >
+            EDIT
+          </StyledButton>
+        </TableCell>
+        <TableCell align="center">
+          <StyledButton
+            color="error"
+            variant="contained"
+            size="small"
+            startIcon={<Delete />}
+          >
+            DELETE
+          </StyledButton>
+        </TableCell>
       </TableRow>
       <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={10}>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={9}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box sx={{ margin: 2 }}>
-              <Typography variant="h6" gutterBottom component="div" sx={{mt: theme.spacing(5) }}>
+              <Typography
+                variant="h6"
+                gutterBottom
+                component="div"
+                sx={{ mt: theme.spacing(5) }}
+              >
                 Subcategories of {props.category.name}
               </Typography>
               <Table size="small" aria-label="purchases">
@@ -120,18 +162,23 @@ function Row(props) {
                   <TableRow>
                     <TableCell align="center">ID</TableCell>
                     <TableCell align="left">NAME</TableCell>
-                    <TableCell align="center">COVER</TableCell>
                     <TableCell align="center">PRODUCTS</TableCell>
                     <TableCell align="center">CREATOR</TableCell>
                     <TableCell align="center">EDIT</TableCell>
                     <TableCell align="center">DELETE</TableCell>
                   </TableRow>
                 </TableHead>
-                <TableBody>
-                 {renderSubcategories()}
-                </TableBody>
+                <TableBody>{renderSubcategories()}</TableBody>
               </Table>
-              <StyledButtonBox><Button variant="contained" size="small" startIcon={<AddCircle />}>Add new subcategory</Button></StyledButtonBox>
+              <StyledButtonBox>
+                <Button
+                  variant="contained"
+                  size="small"
+                  startIcon={<AddCircle />}
+                >
+                  Add new subcategory
+                </Button>
+              </StyledButtonBox>
             </Box>
           </Collapse>
         </TableCell>
@@ -141,46 +188,107 @@ function Row(props) {
 }
 
 export default function Categories(props) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-  
   const MainCategoryTableCell = styled(TableCell)({
-        fontSize: 18,
-  }); 
+    fontSize: 18,
+  });
 
   const StyledButtonBox = styled(Box)({
     marginTop: theme.spacing(2),
     marginBottom: theme.spacing(1),
-});
+  });
+
+  const ModalBox = styled(Paper)({
+    position: "absolute",
+    top: "20%",
+    left: "20%",
+    right: "20%",
+    transform: "translate(-50%, -50%)",
+    width: "60%",
+    boxShadow: 24,
+    p: 4,
+  });
 
   return (
-    <TableContainer component={Box}>
-      <Table aria-label="collapsible table">
-        <TableHead>
-          <TableRow>
-            <TableCell/>
-            <MainCategoryTableCell align="center">ID</MainCategoryTableCell>
-            <MainCategoryTableCell>CATEGORY</MainCategoryTableCell>
-            <MainCategoryTableCell align="center">COVER</MainCategoryTableCell>
-            <MainCategoryTableCell align="center">PRODUCTS</MainCategoryTableCell>
-            <MainCategoryTableCell align="center">SUBCATEGORIES</MainCategoryTableCell>
-            <MainCategoryTableCell align="center">CREATOR</MainCategoryTableCell>
-            <MainCategoryTableCell align="center">EDIT</MainCategoryTableCell>
-            <MainCategoryTableCell align="center">DELETE</MainCategoryTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {props.categories.map((category, index) => {
-            return (
-              <Row
-                key={index}
-                category={category}
-                subcategories={category.subcategories}
-              />
-            );
-          })}
-        </TableBody>
-      </Table>    
-      <StyledButtonBox><Button variant="contained" size="small" startIcon={<AddCircle />}>Add new category</Button></StyledButtonBox>
-    </TableContainer>
+    <Box>
+      <TableContainer component={Box}>
+        <Table aria-label="collapsible table">
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <MainCategoryTableCell align="center">ID</MainCategoryTableCell>
+              <MainCategoryTableCell>CATEGORY</MainCategoryTableCell>
+              <MainCategoryTableCell align="center">
+                PRODUCTS
+              </MainCategoryTableCell>
+              <MainCategoryTableCell align="center">
+                SUBCATEGORIES
+              </MainCategoryTableCell>
+              <MainCategoryTableCell align="center">
+                CREATOR
+              </MainCategoryTableCell>
+              <MainCategoryTableCell align="center">EDIT</MainCategoryTableCell>
+              <MainCategoryTableCell align="center">
+                DELETE
+              </MainCategoryTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.categories.map((category, index) => {
+              return (
+                <Row
+                  key={index}
+                  category={category}
+                  subcategories={category.subcategories}
+                />
+              );
+            })}
+          </TableBody>
+        </Table>
+        <StyledButtonBox>
+          <Button
+            onClick={handleOpen}
+            variant="contained"
+            size="small"
+            startIcon={<AddCircle />}
+          >
+            Add new category
+          </Button>
+        </StyledButtonBox>
+      </TableContainer>
+      <Box>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          slots={{ backdrop: Backdrop }}
+          slotProps={{
+            backdrop: {
+              timeout: 500,
+            },
+          }}
+        >
+          <Zoom in={open}>
+            <ModalBox>
+              <Typography
+                id="transition-modal-title"
+                variant="h6"
+                component="h2"
+              >
+                Text in a modal
+              </Typography>
+              <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+            </ModalBox>
+          </Zoom>
+        </Modal>
+      </Box>
+    </Box>
   );
 }
