9d0fea6f45c2c60d5e7e1117894c1bcd90210acf
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2023-05-13 20:54:03 +0300

    Update BreadcrumbsBar.js

--- ShopHeaven/ClientApp/src/components/BreadcrumbsBar.js
+++ ShopHeaven/ClientApp/src/components/BreadcrumbsBar.js
@@ -1,18 +1,9 @@
-import { Breadcrumbs, Chip, Box } from "@mui/material";
+import { Breadcrumbs, Box, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { theme } from "../theme";
 import { Link } from "react-router-dom";
-import { ArrowRight } from "@mui/icons-material";
 
 export default function BreadcrumbsBar(props) {
-  const StyledChip = styled(Chip)({
-    cursor: "pointer",
-    "&:hover": {
-      backgroundColor: theme.palette.primary.main,
-      color: theme.palette.white.main,
-    },
-  });
-
   const BreadcrumbsWrapper = styled(Box)({
     paddingTop: theme.spacing(11.8),
     width: "80%",
@@ -23,26 +14,26 @@ export default function BreadcrumbsBar(props) {
 
   const CustomBreadcrumbs = styled(Breadcrumbs)({
     display: "flex",
-    justifyContent: "center"
+    justifyContent: "center",
   });
 
   return (
     <BreadcrumbsWrapper>
-    <CustomBreadcrumbs separator={<ArrowRight fontSize="large" color="primary" />} aria-label="breadcrumb">
-      {props.breadcrumbsItems.map((breadcrumb, index) =>
-        index < props.breadcrumbsItems.length - 1 ? (
-          <Link
-            style={{ textDecoration: "none" }}
-            key={index}
-            to={`${breadcrumb.uri}`}
-          >
-            <StyledChip color="primary" variant="outlined" label={breadcrumb.name} />
-          </Link>
-        ) : (
-          <Chip color="primary" variant="outlined" key={index} label={breadcrumb.name} />
-        )
-      )}
-    </CustomBreadcrumbs>
+      <CustomBreadcrumbs separator="›" aria-label="breadcrumb">
+        {props.breadcrumbsItems.map((breadcrumb, index) =>
+          index < props.breadcrumbsItems.length - 1 ? (
+            <Link
+              style={{ textDecoration: "none" }}
+              key={index}
+              to={`${breadcrumb.uri}`}
+            >
+              <Typography>{breadcrumb.name}</Typography>
+            </Link>
+          ) : (
+            <Typography key={index}>{breadcrumb.name}</Typography>
+          )
+        )}
+      </CustomBreadcrumbs>
     </BreadcrumbsWrapper>
   );
 }
