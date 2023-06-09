a769d1db8569f0642cd91fab0b14184a25cdc862
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2023-06-09 17:36:30 +0300

    checkout form first version

--- ShopHeaven/ClientApp/src/components/checkout/Checkout.js
+++ ShopHeaven/ClientApp/src/components/checkout/Checkout.js
@@ -1,9 +1,10 @@
 import { React, useState, useEffect, useRef } from "react";
-import { Box, Grid, Stack, Typography, Button } from "@mui/material";
+import { Box, Grid, Stack, Typography, Button, Paper, Chip } from "@mui/material";
+import { AddCircle } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 import { styled } from "@mui/material/styles";
 import { theme } from "../../theme";
-import { MainWrapper } from "../../styles/styles";
+import { MainWrapper, UniversalInput, InputBox, CompleteActionButton, StyledSelect } from "../../styles/styles";
 import useAuth from "../../hooks/useAuth";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import { ApiEndpoints } from "../../api/endpoints";
@@ -30,9 +31,29 @@ export default function Checkout() {
   const [cartSummary, setCartSummary] = useState();
   const [deleteProductDOMelement, setDeleteProductDOMelement] = useState(false);
 
+  const [orderInfo, setOrderInfo] = useState({
+    recipient: "",
+    phone: "",
+    country: "",
+    city: "",
+    address: "",
+    details: "",
+    shippingMethod: "",
+    paymentMethod: "",
+  });
+
   const { auth } = useAuth();
   const navigate = useNavigate();
   const axiosPrivate = useAxiosPrivate();
+
+  const recipientRef = useRef();
+  const phoneRef = useRef();
+  const countryRef = useRef();
+  const cityRef = useRef();
+  const addressRef = useRef();
+  const detailsRef = useRef();
+  const shippingMethodRef = useRef();
+  const paymentMethodRef = useRef();
 
   const effectRun = useRef(false);
 
@@ -41,13 +62,13 @@ export default function Checkout() {
   }, [])
 
   useEffect(() => {
- 
+
     const controller = new AbortController();
 
-    const getCart = async () => {
+    const getCheckountInfo = async () => {
       try {
         const response = await axiosPrivate.post(
-          ApiEndpoints.carts.getProducts,
+          ApiEndpoints.carts.checkout,
           {
             cartId: auth.cartId,
             userId: auth.userId,
@@ -56,7 +77,7 @@ export default function Checkout() {
             signal: controller.signal,
           }
         );
-        
+
         setProductsInCart(response?.data?.products);
         setCartSummary(response?.data?.summary);
 
@@ -70,7 +91,7 @@ export default function Checkout() {
     };
 
     if (effectRun.current) {
-      getCart();
+      getCheckountInfo();
     }
 
     return () => {
@@ -79,18 +100,148 @@ export default function Checkout() {
     };
   }, []);
 
+  function onCreateOrder(e) {
+    e.preventDefault();
+  }
+
+  const SectionNumberChip = styled(Chip)({
+    background: theme.palette.secondary.main,
+    color: theme.palette.common.white,
+    fontWeight: 500,
+    fontSize: 18,
+  });
+
+  const InputBoxFlex = styled(InputBox)({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 15,
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2)
+  })
+
   return (
     <Box>
       <BreadcrumbsBar breadcrumbsItems={breadcrumbs} />
       <MainWrapper>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={12} md={12} lg={9}>
-             edno
-            </Grid>
-            <Grid item xs={12} sm={12} md={12} lg={3}>
-            dve
-            </Grid>
-          </Grid>
+        <Paper>
+          <form onSubmit={onCreateOrder}>
+            <InputBoxFlex>
+              <SectionNumberChip label={1} />
+              <Typography variant="h6">RECIPIENT</Typography>
+            </InputBoxFlex>
+            <InputBox>
+              <UniversalInput
+                inputRef={recipientRef}
+                label="Recipient"
+                defaultValue={orderInfo.recipient}
+                variant="outlined"
+                placeholder="e.g., Till Lindemann"
+              />
+            </InputBox>
+            <InputBox>
+              <UniversalInput
+                inputRef={phoneRef}
+                label="Phone"
+                defaultValue={orderInfo.phone}
+                variant="outlined"
+                type="tel"
+                placeholder={"e.g., +359854847248"}
+                inputProps={{
+                  step: "0.01",
+                  min: "0.00",
+                }}
+              />
+            </InputBox>
+            <InputBoxFlex>
+              <SectionNumberChip label={2} />
+              <Typography variant="h6">SHIPPING</Typography>
+            </InputBoxFlex>
+            <InputBox sx={{ mb: 1.5 }}>
+              <select
+                style={StyledSelect}
+                ref={shippingMethodRef}
+                name="shippingMethod"
+                defaultValue={orderInfo.shippingMethod}
+              >
+                <option value={""}>
+                  {`${'--- SHIPPING METHOD ---'}`}
+                </option>
+                <option value={"MethodValue"}>
+                  {`${'COURIER'}`}
+                </option>
+              </select>
+            </InputBox> 
+            <InputBox>
+              <UniversalInput
+                inputRef={countryRef}
+                label="Country"
+                defaultValue={orderInfo.country}
+                variant="outlined"
+                placeholder="e.g., Bulgaria"
+              />
+            </InputBox>
+            <InputBox>
+              <UniversalInput
+                inputRef={cityRef}
+                label="City"
+                defaultValue={orderInfo.city}
+                variant="outlined"
+                placeholder="e.g., Sofia"
+              />
+            </InputBox>
+            <InputBox>
+              <UniversalInput
+                inputRef={addressRef}
+                label="Address"
+                defaultValue={orderInfo.address}
+                variant="outlined"
+                placeholder="e.g., Garibaldi Sq. 1"
+              />
+            </InputBox>
+            <InputBox>
+              <UniversalInput
+                multiline
+                rows={4}
+                inputRef={detailsRef}
+                label="Details"
+                defaultValue={orderInfo.details}
+                variant="outlined"
+                placeholder="Additional notes (optional)"
+              />
+            </InputBox>
+            <InputBoxFlex>
+              <SectionNumberChip label={3} />
+              <Typography variant="h6">PAYMENT</Typography>
+            </InputBoxFlex>
+            <InputBox sx={{ mb: 1.5 }}>
+              <select
+                style={StyledSelect}
+                ref={paymentMethodRef}
+                name="paymnetMethod"
+                defaultValue={orderInfo.paymentMethod}
+              >
+                <option value={""}>
+                  {`${'--- PAYMENT METHOD ---'}`}
+                </option>
+                <option value={"PMethodValue"}>
+                  {`${'CARD'}`}
+                </option>
+              </select>
+            </InputBox>
+            <InputBox>
+              <CompleteActionButton
+                color="secondary"
+                startIcon={<AddCircle />}
+                type="submit"
+                size="large"
+                variant="contained"
+              >
+                Send Order
+              </CompleteActionButton>
+            </InputBox>
+          </form>
+        </Paper>
       </MainWrapper>
     </Box>
   );
