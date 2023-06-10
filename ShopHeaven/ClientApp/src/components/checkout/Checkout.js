6b80f09013ac6c6a84b0632eb36819690bcf89fc
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2023-06-10 12:38:10 +0300

    order price introduced

--- ShopHeaven/ClientApp/src/components/checkout/Checkout.js
+++ ShopHeaven/ClientApp/src/components/checkout/Checkout.js
@@ -1,5 +1,6 @@
 import { React, useState, useEffect, useRef } from "react";
 import { Box, Grid, Stack, Typography, Button, Paper, Chip } from "@mui/material";
+import { AddShoppingCart } from "@mui/icons-material";
 import { AddCircle } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 import { styled, alpha } from "@mui/material/styles";
@@ -9,6 +10,7 @@ import useAuth from "../../hooks/useAuth";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import { ApiEndpoints } from "../../api/endpoints";
 import { cartPath, checkoutPath } from "../../constants";
+import useAppSettings from "../../hooks/useAppSettings";
 import BreadcrumbsBar from "../common/BreadcrumbsBar";
 
 const breadcrumbs = [
@@ -43,6 +45,7 @@ export default function Checkout() {
   });
 
   const { auth } = useAuth();
+  const { appSettings } = useAppSettings();
   const navigate = useNavigate();
   const axiosPrivate = useAxiosPrivate();
 
@@ -145,6 +148,43 @@ export default function Checkout() {
     })
   }
 
+  const RegularPriceHolder = styled(Box)({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    color: "gray",
+    fontWeight: 500,
+    fontSize: 20,
+    textAlign: "right",
+    marginTop: theme.spacing(2),
+  });
+
+
+  const ActionButton = styled(Button)({
+    paddingTop: theme.spacing(1.5),
+    paddingBottom: theme.spacing(1.5),
+    width: "100%",
+    marginTop: theme.spacing(3),
+  });
+
+  const DiscountHolder = styled(Box)({
+    display: "flex",
+    color: "gray",
+    alignItems: "center",
+    justifyContent: "space-between",
+  });
+
+  const PriceHolder = styled(Box)({
+    display: "flex",
+    [theme.breakpoints.up("lg")]: {
+      display: "block",
+      margin: "auto",
+      textAlign: "center",
+    },
+    alignItems: "center",
+    justifyContent: "space-between",
+  });
+
   const SectionNumberChip = styled(Chip)({
     background: theme.palette.secondary.main,
     color: theme.palette.common.white,
@@ -170,118 +210,161 @@ export default function Checkout() {
     <Box>
       <BreadcrumbsBar breadcrumbsItems={breadcrumbs} />
       <MainWrapper>
-        <Paper sx={{p:2, backgroundColor: alpha(theme.palette.primary.main, 0.1)}}>
-        <Typography textAlign={"center"} variant="h4" fontWeight={500}>ORDER INFO</Typography>
+        <Paper sx={{ p: 2, backgroundColor: alpha(theme.palette.primary.main, 0.1) }}>
+          <Typography textAlign={"center"} variant="h4" fontWeight={500}>ORDER INFO</Typography>
           <form onSubmit={onCreateOrder}>
             <SectionWrapper elevation={0}>
-            <InputBoxFlex>
-              <SectionNumberChip label={1} />
-              <Typography variant="h6">RECIPIENT</Typography>
-            </InputBoxFlex>
-            <InputBox>
-              <UniversalInput
-                inputRef={recipientRef}
-                label="Recipient"
-                defaultValue={orderInfo.recipient}
-                variant="outlined"
-                placeholder="e.g., Till Lindemann"
-              />
-            </InputBox>
-            <InputBox>
-              <UniversalInput
-                inputRef={phoneRef}
-                label="Phone"
-                defaultValue={orderInfo.phone}
-                variant="outlined"
-                type="tel"
-                placeholder={"e.g., +359854847248"}
-                inputProps={{
-                  step: "0.01",
-                  min: "0.00",
-                }}
-              />
-            </InputBox>
+              <InputBoxFlex>
+                <SectionNumberChip label={1} />
+                <Typography variant="h6">RECIPIENT</Typography>
+              </InputBoxFlex>
+              <InputBox>
+                <UniversalInput
+                  inputRef={recipientRef}
+                  label="Recipient"
+                  defaultValue={orderInfo.recipient}
+                  variant="outlined"
+                  placeholder="e.g., Till Lindemann"
+                />
+              </InputBox>
+              <InputBox>
+                <UniversalInput
+                  inputRef={phoneRef}
+                  label="Phone"
+                  defaultValue={orderInfo.phone}
+                  variant="outlined"
+                  type="tel"
+                  placeholder={"e.g., +359854847248"}
+                  inputProps={{
+                    step: "0.01",
+                    min: "0.00",
+                  }}
+                />
+              </InputBox>
             </SectionWrapper>
             <SectionWrapper elevation={0}>
-            <InputBoxFlex>
-              <SectionNumberChip label={2} />
-              <Typography variant="h6">SHIPPING</Typography>
-            </InputBoxFlex>
-            <InputBox sx={{ mb: 1.5 }}>
-              <select
-                style={StyledSelect}
-                ref={shippingMethodRef}
-                name="shippingMethod"
-                defaultValue={orderInfo.shippingMethod}
-              >
-                <option value={""}>
-                  {`${'--- SHIPPING METHOD ---'}`}
-                </option>
-                <option value={"MethodValue"}>
-                  {`${'COURIER'}`}
-                </option>
-              </select>
-            </InputBox> 
-            <InputBox>
-              <UniversalInput
-                inputRef={countryRef}
-                label="Country"
-                defaultValue={orderInfo.country}
-                variant="outlined"
-                placeholder="e.g., Bulgaria"
-              />
-            </InputBox>
-            <InputBox>
-              <UniversalInput
-                inputRef={cityRef}
-                label="City"
-                defaultValue={orderInfo.city}
-                variant="outlined"
-                placeholder="e.g., Sofia"
-              />
-            </InputBox>
-            <InputBox>
-              <UniversalInput
-                inputRef={addressRef}
-                label="Address"
-                defaultValue={orderInfo.address}
-                variant="outlined"
-                placeholder="e.g., Garibaldi Sq. 1"
-              />
-            </InputBox>
-            <InputBox>
-              <UniversalInput
-                multiline
-                rows={4}
-                inputRef={detailsRef}
-                label="Details"
-                defaultValue={orderInfo.details}
-                variant="outlined"
-                placeholder="Additional notes (optional)"
-              />
-            </InputBox>
+              <InputBoxFlex>
+                <SectionNumberChip label={2} />
+                <Typography variant="h6">SHIPPING</Typography>
+              </InputBoxFlex>
+              <InputBox sx={{ mb: 1.5 }}>
+                <select
+                  style={StyledSelect}
+                  ref={shippingMethodRef}
+                  name="shippingMethod"
+                  defaultValue={orderInfo.shippingMethod}
+                >
+                  <option value={""}>
+                    {`${'--- SHIPPING METHOD ---'}`}
+                  </option>
+                  <option value={"MethodValue"}>
+                    {`${'COURIER'}`}
+                  </option>
+                </select>
+              </InputBox>
+              <InputBox>
+                <UniversalInput
+                  inputRef={countryRef}
+                  label="Country"
+                  defaultValue={orderInfo.country}
+                  variant="outlined"
+                  placeholder="e.g., Bulgaria"
+                />
+              </InputBox>
+              <InputBox>
+                <UniversalInput
+                  inputRef={cityRef}
+                  label="City"
+                  defaultValue={orderInfo.city}
+                  variant="outlined"
+                  placeholder="e.g., Sofia"
+                />
+              </InputBox>
+              <InputBox>
+                <UniversalInput
+                  inputRef={addressRef}
+                  label="Address"
+                  defaultValue={orderInfo.address}
+                  variant="outlined"
+                  placeholder="e.g., Garibaldi Sq. 1"
+                />
+              </InputBox>
+              <InputBox>
+                <UniversalInput
+                  multiline
+                  rows={4}
+                  inputRef={detailsRef}
+                  label="Details"
+                  defaultValue={orderInfo.details}
+                  variant="outlined"
+                  placeholder="Additional notes (optional)"
+                />
+              </InputBox>
             </SectionWrapper>
             <SectionWrapper elevation={0}>
-            <InputBoxFlex>
-              <SectionNumberChip label={3} />
-              <Typography variant="h6">PAYMENT</Typography>
-            </InputBoxFlex>
-            <InputBox sx={{ mb: 1.5 }}>
-              <select
-                style={StyledSelect}
-                ref={paymentMethodRef}
-                name="paymnetMethod"
-                defaultValue={orderInfo.paymentMethod}
-              >
-                <option value={""}>
-                  {`${'--- PAYMENT METHOD ---'}`}
-                </option>
-                <option value={"PMethodValue"}>
-                  {`${'CARD'}`}
-                </option>
-              </select>
-            </InputBox>
+              <InputBoxFlex>
+                <SectionNumberChip label={3} />
+                <Typography variant="h6">PAYMENT</Typography>
+              </InputBoxFlex>
+              <InputBox sx={{ mb: 1.5 }}>
+                <select
+                  style={StyledSelect}
+                  ref={paymentMethodRef}
+                  name="paymnetMethod"
+                  defaultValue={orderInfo.paymentMethod}
+                >
+                  <option value={""}>
+                    {`${'--- PAYMENT METHOD ---'}`}
+                  </option>
+                  <option value={"PMethodValue"}>
+                    {`${'CARD'}`}
+                  </option>
+                </select>
+              </InputBox>
             </SectionWrapper>
+            <Paper sx={{ p: 2 }}>
+              <Stack spacing={1}>
+                <Typography
+                  align="center"
+                  variant="h6"
+                  fontWeight={400}
+                  sx={{ mb: 1 }}
+                >
+                  ORDER SUMMARY:
+                </Typography>
+                <RegularPriceHolder>
+                  <Typography>Regular price:</Typography>
+                  <Typography sx={{ textDecoration: "line-through" }}>
+                    {appSettings.appCurrency.code}{" "}
+                    {10}
+                  </Typography>
+                </RegularPriceHolder>
+                <DiscountHolder>
+                  <Typography>Discount:</Typography>
+                  <Typography>
+                    {appSettings.appCurrency.code} -
+                    {10}
+                  </Typography>
+                </DiscountHolder>
+                <DiscountHolder>
+                  <Typography>Coupon discount:</Typography>
+                  <Typography>
+                    {`${appSettings.appCurrency.code} ${200} (-${12}%)`}
+                  </Typography>
+                </DiscountHolder>
+                <PriceHolder>
+                  <Typography sx={{ fontWeight: 600, fontSize: 18 }}>
+                    TOTAL PRICE:
+                  </Typography>
+                  <Typography
+                    variant="h5"
+                    sx={{ fontWeight: 500, color: theme.palette.error.main }}
+                  >
+                    EUR 350
+                  </Typography>
+                </PriceHolder>
+              </Stack>
+            </Paper>
             <InputBox>
               <CompleteActionButton
                 color="secondary"
