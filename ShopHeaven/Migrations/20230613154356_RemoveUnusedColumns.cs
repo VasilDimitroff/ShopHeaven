93d529b1d939ec2ba18fea67febf3f46f61341fb
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2023-06-13 18:57:28 +0300

    db schema corrected

---
+++ ShopHeaven/Migrations/20230613154356_RemoveUnusedColumns.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopHeaven.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUnusedColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Премахване на външния ключ, свързан с колоната "PaymentId" в таблицата "PaymentSessions"
+            migrationBuilder.DropForeignKey(
+                name: "FK_PaymentSessions_Payments_PaymentId",
+                table: "PaymentSessions");
+
+            // Премахване на индекса, свързан с колоната "PaymentId" в таблицата "PaymentSessions"
+            migrationBuilder.DropIndex(
+                name: "IX_PaymentSessions_PaymentId",
+                table: "PaymentSessions");
+
+            // Премахване на колоната "PaymentId" в таблицата "PaymentSessions"
+            migrationBuilder.DropColumn(
+                name: "PaymentId",
+                table: "PaymentSessions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ ShopHeaven/Migrations/20230613155411_RemoveUnusedIndexPayment.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopHeaven.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUnusedIndexPayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Премахване на външния ключ, свързан с колоната "PaymentSessionId" в таблицата "Payments"
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_PaymentSessions_PaymentSessionId",
+                table: "Payments");
+
+            // Премахване на индекса, свързан с колоната "PaymentSessionId" в таблицата "Payments"
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_PaymentSessionId",
+                table: "Payments");
+
+            // Премахване на колоната "PaymentSessionId" в таблицата "Payments"
+            migrationBuilder.DropColumn(
+                name: "PaymentSessionId",
+                table: "Payments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
